500e035f0e40d3f609843257f4b37c86a884318a
Amogh Sharma <amoghsharma@dhcp-v880-17b-00356.1Xwireless.unc.edu>
2017-09-29 15:15:00 -0400

    edited NC county data to include all counties

--- examples/node-example/src/data/ncCountyData.js
+++ examples/node-example/src/data/ncCountyData.js
@@ -10,5 +10,593 @@ export default [
     county_name: "Cherokee",
     amount_w: 91.1,
     amount_m: 93.88
+  },
+    {
+    county_id: 2,
+    county_name: "Avery",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 3,
+    county_name: "Watauga",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 4,
+    county_name: "Hoke",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 5,
+    county_name: "Rowan",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 6,
+    county_name: "Bertie",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 7,
+    county_name: "Johnston",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 8,
+    county_name: "Greene",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 9,
+    county_name: "Halifax",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 10,
+    county_name: "Haywood",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 11,
+    county_name: "Bladen",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 12,
+    county_name: "Clay",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 13,
+    county_name: "Currituck",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 14,
+    county_name: "Caswell",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 15,
+    county_name: "Craven",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 16,
+    county_name: "Graham",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 17,
+    county_name: "Camden",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 18,
+    county_name: "Guilford",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 19,
+    county_name: "Harnett",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 20,
+    county_name: "Pitt",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 21,
+    county_name: "Duplin",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 22,
+    county_name: "Catawba",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 23,
+    county_name: "Pamlico",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 24,
+    county_name: "Columbus",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 25,
+    county_name: "Nash",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 26,
+    county_name: "Surry",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 27,
+    county_name: "Anson",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 28,
+    county_name: "Montgomery",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 29,
+    county_name: "Durham",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 30,
+    county_name: "Henderson",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 31,
+    county_name: "Chowan",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 32,
+    county_name: "Transylvania",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 33,
+    county_name: "Pasquotank",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 34,
+    county_name: "Warren",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 35,
+    county_name: "Wilkes",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 36,
+    county_name: "Lincoln",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 37,
+    county_name: "Wayne",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 38,
+    county_name: "Jackson",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 39,
+    county_name: "Sampson",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 40,
+    county_name: "Hyde",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 41,
+    county_name: "New Hanover",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 42,
+    county_name: "Madison",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 43,
+    county_name: "Martin",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 44,
+    county_name: "Orange",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 45,
+    county_name: "Granville",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 46,
+    county_name: "Forsyth",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 47,
+    county_name: "Franklin",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 48,
+    county_name: "Lenoir",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 49,
+    county_name: "Caldwell",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 50,
+    county_name: "Alamance",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 51,
+    county_name: "McDowell",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 52,
+    county_name: "Burke",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 53,
+    county_name: "Chatham",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 54,
+    county_name: "Vance",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 55,
+    county_name: "Davie",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 56,
+    county_name: "Stanly",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 57,
+    county_name: "Ashe",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 58,
+    county_name: "Mitchell",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 59,
+    county_name: "Richmond",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 60,
+    county_name: "Jones",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 61,
+    county_name: "Northampton",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 62,
+    county_name: "Alexander",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 63,
+    county_name: "Gates",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 64,
+    county_name: "Davidson",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 65,
+    county_name: "Person",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 66,
+    county_name: "Washington",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 67,
+    county_name: "Robeson",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 68,
+    county_name: "Beaufort",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 69,
+    county_name: "Perquimans",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 70,
+    county_name: "Dare",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 71,
+    county_name: "Scotland",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 72,
+    county_name: "Onslow",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 73,
+    county_name: "Rutherford",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 74,
+    county_name: "Alleghany",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 75,
+    county_name: "Tyrrell",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 76,
+    county_name: "Rockingham",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 77,
+    county_name: "Macon",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 78,
+    county_name: "Stokes",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 79,
+    county_name: "Mecklenburg",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 80,
+    county_name: "Union",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 81,
+    county_name: "Wake",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 82,
+    county_name: "Cumberland",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 83,
+    county_name: "Swain",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 84,
+    county_name: "Randolph",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 85,
+    county_name: "Yancey",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 86,
+    county_name: "Wilson",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 87,
+    county_name: "Yadkin",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 88,
+    county_name: "Brunswick",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 89,
+    county_name: "Cleveland",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 90,
+    county_name: "Moore",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 91,
+    county_name: "Pender",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+    {
+    county_id: 92,
+    county_name: "Hertford",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+  {
+    county_id: 93,
+    county_name: "Iredell",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+  {
+    county_id: 94,
+    county_name: "Gaston",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+  {
+    county_id: 95,
+    county_name: "Edgecombe",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+  {
+    county_id: 96,
+    county_name: "Cabarrus",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+  {
+    county_id: 97,
+    county_name: "Buncombe",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+  {
+    county_id: 98,
+    county_name: "Lee",
+    amount_w: 91.1,
+    amount_m: 93.88
+  },
+  {
+    county_id: 99,
+    county_name: "Carteret",
+    amount_w: 91.1,
+    amount_m: 93.88
   }
 ]
